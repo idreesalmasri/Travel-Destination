23566b85547594dc1d53ffffaeb968e260ac9298
idreesalmasri <idrees.massri95@gmail.com>
2022-02-14 01:19:22 +0200

    done

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,11 +1,11 @@
-
+import "./Footer.css";
 export default function Footer(){
     return (
-        <>
+        <div id="footer">
         <a href="">Facebook  </a>
         <a href="">instagram  </a>
         <a href="">twitter  </a>
         <p>"Author : Idrees" </p>
-        </>
+        </div>
     );
 }
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,8 +1,8 @@
-
+import "./Header.css";
 export default function Header(){
     return (
         <>
-        <h1>Travel Destination web application</h1>
+        <h1 id="header">Travel Destination web application</h1>
         </>
     );
 }
