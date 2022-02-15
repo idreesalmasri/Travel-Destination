aedde7596f385c294c4a57f98b207532abbbde77
idreesalmasri <idrees.massri95@gmail.com>
2022-02-15 12:38:43 +0200

    not finish

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import Home from './components/home/Home.js';
 import './App.css';
+import { Routes, Route, Link } from "react-router-dom";
 const data=require("./data/db.json")
 function App() {
   return (
     <>
-    <Home myData={data}/>
+    {/* <Home myData={data}/> */}
+    <Routes>
+        <Route path="/" element={<Home myData={data}/>} />
+        {/* <Route path="about" element={<About />} /> */}
+      </Routes>
     </>
   );
 }
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,8 +1,10 @@
 import "./Header.css";
+import Navbar from '../Navbar/Navbar';
 export default function Header(){
     return (
         <>
         <h1 id="header">Travel Destination web application</h1>
+        <Navbar/>
         </>
     );
 }
